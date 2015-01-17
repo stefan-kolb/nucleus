eafa79e82f754dce82d29565110382019452a3b6
Cedric Röck <git@roecky.net>
2015-01-17 18:58:07 +0100

    Added requirements for API v1 and an adapter that shall provide those requirements

---
+++ app/adapters/v1/cloudFoundry/cloud_foundry_adapter.rb
@@ -0,0 +1,20 @@
+module Paasal
+  module Adapters
+    class CloudFoundryAdapter < Paasal::Adapters::BaseAdapter
+      # TODO how to assert that the class implements all required operations?
+
+      def start
+
+      end
+
+      def stop
+
+      end
+
+      def restart
+
+      end
+
+    end
+  end
+end
