b0f058c8617c9b947138d4c02c95c15c2db76833
Cedric Röck <git@roecky.net>
2015-01-17 19:37:25 +0100

    changed the error request logger to dispatch a call to the log but still print it to the console

--- app/middleware/error_request_logger.rb
+++ app/middleware/error_request_logger.rb
@@ -12,15 +12,57 @@ module Paasal
       end
 
       def call(env)
-        # apply the error logger
+        # create the error logger
         error_logger = ::File.new(@file, 'a+')
         error_logger.sync = true
-        env['rack.errors'] =  error_logger
+        # assign dispatcher to be used for printing errors
+        dispatcher = ErrorRequestDispatcher.new([error_logger, env['rack.errors']])
+        env['rack.errors'] = dispatcher
         # execute call
         status, headers, body = @app.call(env)
         # send the response
         [status, headers, body]
       end
     end
+
+    # Dispatch the call (write << puts p pp) to all loggers
+     class ErrorRequestDispatcher
+        def initialize(loggers)
+          @loggers = loggers
+        end
+
+       def puts(msg)
+         @loggers.each do |logger|
+           if logger.respond_to?(:puts)
+             logger.puts(msg)
+           end
+         end
+       end
+
+        def write(msg)
+          @loggers.each do |logger|
+            if logger.respond_to?(:write)
+              logger.write(msg)
+            end
+          end
+        end
+
+        def <<(msg)
+          @loggers.each do |logger|
+            if logger.respond_to?(:<<)
+              logger << msg
+            end
+          end
+        end
+
+        def flush
+          @loggers.each do |logger|
+            if logger.respond_to?(:flush)
+              logger.flush
+            end
+          end
+        end
+
+     end
   end
 end
