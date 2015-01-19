d309add894c71052d5b232481c207428d6686067
Cedric Röck <git@roecky.net>
2015-01-19 20:06:43 +0100

    added thread_config_accessor

---
+++ app/core/thread_config_accessor.rb
@@ -0,0 +1,86 @@
+# Redefines the Class and provides the thread_lib_accessor.
+#
+# If invoked with a variable name, the class and its instances can be used
+# as a key-value config for that variable name.
+# E.g.:
+#    class ThreadedConfig
+#      thread_config_accessor :some_setting, :default => 5
+#    end
+#
+#    class TestThreadedConfig < Test::Unit::TestCase
+#      def test_that_the_accessors_work!
+#        # create instance
+#        config = ThreadedConfig.new
+#        # assign value to the class
+#        ThreadedConfig.setting_a = 1
+#
+#        # value is equal for both, class and instance
+#        assert_equal 1, ThreadedConfig.setting_a
+#        assert_equal 1, config.setting_a
+#
+#        # create new Thread, which should NOT have the values assigned
+#        Thread.new {
+#          config.setting_a = 2
+#          assert_equal 2, ThreadedConfig.setting_a
+#          assert_equal 2, config.setting_a
+#        }.join
+#
+#        # create new Thread and assert the default value was assigned
+#        Thread.new { assert_equal 5, ThreadedConfig.setting_a }.join
+#
+#        assert_equal 1, ThreadedConfig.setting_a
+#      end
+#    end
+#
+# By coderrr (Steve), see https://coderrr.wordpress.com/2008/04/10/lets-stop-polluting-the-threadcurrent-hash/
+class Class
+
+  # Binds accessors to the class and its instances and allows to use them as thread-bound config
+  def thread_config_accessor name, options = {}
+    mod = Module.new
+    mod.module_eval do
+      class_variable_set :"@@#{name}", Hash.new {|h,k| h[k] = options[:default] }
+    end
+
+    # use finalizer to prevent memory leaks and clean-up when threads die
+    mod.module_eval %{
+      FINALIZER = lambda {|id| @@#{name}.delete id }
+
+      def #{name}
+        @@#{name}[Thread.current.object_id]
+      end
+
+      def #{name}=(val)
+        ObjectSpace.define_finalizer Thread.current, FINALIZER  unless @@#{name}.has_key? Thread.current.object_id
+        @@#{name}[Thread.current.object_id] = val
+      end
+    }
+
+    class_eval do
+      include mod
+      extend mod
+    end
+  end
+
+  # Binds accessors to the class and its instances and allows to use them as (read-only) thread-bound config
+  def thread_config_accessor_readonly name, options = {}
+    mod = Module.new
+    mod.module_eval do
+      class_variable_set :"@@#{name}", Hash.new {|h,k| h[k] = options[:default] }
+    end
+
+    # use finalizer to prevent memory leaks and clean-up when threads die
+    mod.module_eval %{
+      FINALIZER = lambda {|id| @@#{name}.delete id }
+
+      def #{name}
+        @@#{name}[Thread.current.object_id]
+      end
+    }
+
+    class_eval do
+      include mod
+      extend mod
+    end
+  end
+end
---
+++ spec/common/thread_config_accessor_spec.rb
@@ -0,0 +1,101 @@
+require_relative '../../app/core/thread_config_accessor'
+
+class ThreadedConfig
+  thread_config_accessor :setting_a, :default => 5
+end
+
+class ThreadedConfigParentA
+end
+
+class ThreadedConfigParentB
+  thread_config_accessor :setting_a, :default => 13
+end
+
+class ThreadedConfigChildA < ThreadedConfigParentA
+  thread_config_accessor :setting_a, :default => 11
+end
+
+class ThreadedConfigChildB < ThreadedConfigParentB
+end
+
+class ThreadedConfigReadOnly
+  thread_config_accessor_readonly :setting_a, :default => 7
+end
+
+describe 'config values accessibility' do
+
+  it 'for config defined in base class shall be available to inheriting classes' do
+    child = ThreadedConfigChildB.new
+    expect(child.setting_a).to eql 13
+    child.setting_a = 17
+    expect(child.setting_a).to eql 17
+    expect(child.setting_a).to eql ThreadedConfigParentB.setting_a
+    ThreadedConfigParentB.setting_a = 19
+    expect(child.setting_a).to eql ThreadedConfigParentB.setting_a
+    expect(child.setting_a).to eql 19
+  end
+
+  it 'for config defined in inheriting class shall not be available to parent classes' do
+    child = ThreadedConfigChildA.new
+    expect(child.setting_a).to eql 11
+    expect{ThreadedConfigParentA.setting_a}.to raise_error(NoMethodError)
+  end
+
+end
+
+describe 'thread_config_accessor' do
+
+  before :all do
+    # create instance
+    @config = ThreadedConfig.new
+    # assign value to the class
+    ThreadedConfig.setting_a = 1
+  end
+
+  it 'should have the same value on the class and instance' do
+    expect(@config.setting_a).to eql 1
+    expect(@config.setting_a).to eql ThreadedConfig.setting_a
+  end
+
+  it 'should have the different values in a new thread' do
+    Thread.new {
+      @config.setting_a = 2
+      expect(@config.setting_a).to eql 2
+      expect(@config.setting_a).to eql ThreadedConfig.setting_a
+    }.join
+  end
+
+  it 'should have the default values in a new thread' do
+    Thread.new {
+      expect(@config.setting_a).to eql 5
+      expect(@config.setting_a).to eql ThreadedConfig.setting_a
+    }.join
+  end
+
+end
+
+describe 'thread_config_accessor_readonly' do
+
+  before :all do
+    # create instance
+    @config = ThreadedConfigReadOnly.new
+  end
+
+  it 'should have the same value on the class and instance' do
+    expect(@config.setting_a).to eql 7
+    expect(@config.setting_a).to eql ThreadedConfigReadOnly.setting_a
+  end
+
+  it 'should keep the same default values in a new thread' do
+    Thread.new {
+      expect(@config.setting_a).to eql 7
+      expect(@config.setting_a).to eql ThreadedConfigReadOnly.setting_a
+    }.join
+  end
+
+  it 'should not allow to change the default values' do
+    Thread.new {
+      expect{@config.setting_a = 10}.to raise_error
+    }.join
+  end
+end
