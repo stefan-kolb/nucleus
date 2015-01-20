fc461bd26a89b592fce5d800458f2b9324c1afc3
Cedric Röck <git@roecky.net>
2015-01-20 15:23:18 +0100

    spec helper update, distinguish between local and CI tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,19 @@
 $:<< File.join(File.dirname(__FILE__), '..')
 
-if ENV['CI']
+if ENV['CODECLIMATE_REPO_TOKEN']
   require 'codeclimate-test-reporter'
   CodeClimate::TestReporter.start
 else
   require 'simplecov'
-  SimpleCov.start
+  SimpleCov.start do
+    add_group 'Adapters', 'app/adapters'
+    add_group 'API versions', 'app/api/versions'
+    add_group 'API entities', 'app/api/entities'
+    add_group 'API helpers', 'app/api/helpers'
+    add_group 'Core', 'app/core'
+    add_group 'Middleware', 'app/middleware'
+    add_group 'Models', 'app/models'
+  end
 end
+
+require_relative '../scripts/load_app'
