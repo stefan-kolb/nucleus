2ea43c664b814458e7598a48345d13f3e62786f5
Cedric Röck <git@roecky.net>
2015-01-23 14:52:09 +0100

    Added all remaing error responses

--- app/api/error_responses.rb
+++ app/api/error_responses.rb
@@ -1,8 +1,11 @@
 module Paasal
   module ErrorResponses
     def self.standard_responses
-      [[401, 'Unauthorized', Paasal::API::Models::Error],
-       [404, 'Resource not found', Paasal::API::Models::Error]]
+      [[400, 'Bad Request', Paasal::API::Models::Error],
+       [401, 'Unauthorized', Paasal::API::Models::Error],
+       [403, 'Forbidden', Paasal::API::Models::Error],
+       [404, 'Resource not found', Paasal::API::Models::Error],
+       [500, 'Internal processing error', Paasal::API::Models::Error]]
     end
   end
 end
