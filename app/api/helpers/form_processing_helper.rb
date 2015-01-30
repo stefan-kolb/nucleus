473da0b70a611bd4ec89db8d582470d050978372
Cedric Röck <git@roecky.net>
2015-01-30 22:25:16 +0100

    Added missing helper for PATCH commands

---
+++ app/api/helpers/form_processing_helper.rb
@@ -0,0 +1,22 @@
+module Paasal
+  module FormProcessingHelper
+    extend Grape::API::Helpers
+
+    def update_fields(entity, entity_documentation, fields)
+      # update all fields, but only with allowed values (according to the doc)
+      fields.each do |key, value|
+        # only continue if the value can be assigned to the entity
+        next unless entity.respond_to? key
+        allow_blank = entity_documentation[key.to_sym][:allow_blank]
+        # allow_nil = entity_documentation[key][:required]
+
+        # currently no nil values allowed
+        next if value.nil?
+        # if value is blank, blank must be allowed
+        next if value.empty? && !allow_blank
+        # finally update the value
+        entity.send("#{key}=", value)
+      end unless fields.nil?
+    end
+  end
+end
