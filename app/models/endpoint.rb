3613ef0ef45c4b991b546a006e109d700a33aa08
Cedric Röck <git@roecky.net>
2015-02-06 10:56:26 +0100

    Added 'trust' option to endpoints, which says that we do not need to verify SSL certificates

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -5,6 +5,7 @@ module Paasal
     attr_accessor :provider
     attr_accessor :name             # text
     attr_accessor :url              # str
+    attr_accessor :trust            # bool
 
     def initialize(hash = nil)
       return if hash.nil?
@@ -12,11 +13,14 @@ module Paasal
       @name = hash[:name]
       @url = hash[:url]
       @id = hash[:id]
+      @trust = hash.key? :trust ? hash[:trust] : false
     end
 
     def representation
       msg = "Endpoint: #{@name}"
       msg << " [#{@url}]" unless @url.nil? || @url.empty?
+      msg << ' *trusted*' if @trust
+      msg
     end
   end
 end
