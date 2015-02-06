b3aaaeb7be7146891f506b5483ad6f9c09503b3f
Cedric Röck <git@roecky.net>
2015-02-06 09:30:25 +0100

    Added missing basic references entity

---
+++ app/api/entities/links/basic_references.rb
@@ -0,0 +1,17 @@
+module Paasal
+  module API
+    module Models
+      class BasicReferences < Grape::Entity
+        def self.entity_name
+          'BasicReferences'
+        end
+
+        expose :self, documentation: { type: 'Link', desc: 'Self-reference', required: true },
+               using: Paasal::API::Models::Link
+
+        expose :parent, documentation: { type: 'Link', desc: 'Reference to parent resource', required: true},
+               using: Paasal::API::Models::Link
+      end
+    end
+  end
+end
