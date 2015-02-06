0d0c318ee343a28ee831500ff04f1c76c7267a4a
Cedric Röck <git@roecky.net>
2015-02-06 09:36:07 +0100

    Updated provider spec to match new listing methods

--- spec/integration/api/providers_spec.rb
+++ spec/integration/api/providers_spec.rb
@@ -11,8 +11,8 @@ describe Paasal::API::V1::Providers do
   let!(:endpoint_b) { create(:endpoint, provider: provider_a.id) }
   let!(:endpoint_c) { create(:endpoint, provider: provider_b.id) }
 
-  context 'GET /providers response' do
-    before { get '/providers' }
+  describe 'GET /vendors/:vendor_id/providers response' do
+    before { get "/vendors/#{vendor.id}/providers" }
     include_examples 'a valid GET request'
     it 'has provider-list like structure' do
       expect_json_types(size: :int, providers: :array_of_objects, _links: :object)
@@ -30,7 +30,30 @@ describe Paasal::API::V1::Providers do
     end
   end
 
-  context 'GET /providers/:id' do
+  describe 'GET /vendors/:vendor_id/providers response (repeated)' do
+    before {
+      get "/vendors/#{vendor.id}/providers"
+      # 2nd call, previous issue caused providers the be shown only once
+      get "/vendors/#{vendor.id}/providers"
+    }
+    include_examples 'a valid GET request'
+    it 'has provider-list like structure' do
+      expect_json_types(size: :int, providers: :array_of_objects, _links: :object)
+    end
+
+    # TODO: contains all required fields
+
+    it 'shows the retrieved number of providers' do
+      expect(json_body[:size]).to eq 2
+      expect_json_sizes(providers: 2)
+    end
+    it 'lists all providers' do
+      expect(json_body[:providers][0][:id]).to eq provider_a.id
+      expect(json_body[:providers][1][:id]).to eq provider_b.id
+    end
+  end
+
+  describe 'GET /providers/:id' do
     before { get "/providers/#{provider_a.id}" }
     include_examples 'a valid GET request'
     it 'has the structure of an provider object' do
@@ -42,36 +65,36 @@ describe Paasal::API::V1::Providers do
     end
   end
 
-  context 'DELETE /providers/:id' do
+  describe 'DELETE /providers/:id' do
     before { delete "/providers/#{provider_a.id}" }
     include_examples 'a valid DELETE request'
 
-    context 'causes the provider list' do
-      before { get '/providers' }
-      it 'to not include the provider anymore' do
+    describe 'proves that the provider list' do
+      before { get "/vendors/#{vendor.id}/providers" }
+      it 'does not include the provider anymore' do
         expect(json_body[:size]).to eq 1
         expect_json_sizes(providers: 1)
       end
     end
   end
 
-  context 'PATCH /providers/:id' do
+  describe 'PATCH /providers/:id' do
     let!(:updated_name) { Faker::Internet.slug }
 
     context 'with a valid body' do
       before { patch "/providers/#{provider_a.id}", provider: { name: updated_name } }
       include_examples 'a valid PATCH request'
 
-      context 'causes the provider retrieval' do
+      describe 'proves that the provider retrieval' do
         before { get "/providers/#{provider_a.id}" }
         include_examples 'a valid GET request'
-        it 'to show the requested resource' do
+        it 'shows the requested resource' do
           expect(json_body[:id]).to eq provider_a.id
         end
-        it 'to show the updated values' do
+        it 'shows the updated values' do
           expect(json_body[:name]).to eq updated_name
         end
-        it 'to show an updated updated_at timestamp' do
+        it 'shows an updated updated_at timestamp' do
           expect(json_body[:updated_at]).to be >= provider_a.updated_at
         end
         it 'has the original value of fields that were not changed' do
@@ -87,7 +110,7 @@ describe Paasal::API::V1::Providers do
     end
   end
 
-  context 'POST /vendors/:id/providers' do
+  describe 'POST /vendors/:id/providers' do
     context 'with a valid body' do
       let!(:p_name) { Faker::Internet.slug }
       before { post "/vendors/#{vendor.id}/providers", provider: { name: p_name } }
@@ -97,17 +120,17 @@ describe Paasal::API::V1::Providers do
                           endpoints: :array_of_objects, _links: :object)
       end
 
-      context 'causes the' do
-        context 'provider list' do
-          before { get '/providers' }
+      describe 'proves that the' do
+        describe 'provider list' do
+          before { get "/vendors/#{vendor.id}/providers" }
           include_examples 'a valid GET request'
-          it 'to include one more provider' do
+          it 'includes one additional provider' do
             expect(json_body[:size]).to eq 3
             expect_json_sizes(providers: 3)
           end
         end
 
-        context 'provider retrieval of the created resource' do
+        describe 'provider retrieval of the created resource' do
           before { get "/providers/#{json_body[:id]}" }
           include_examples 'a valid GET request'
           it 'shows a provider with the correct values' do
@@ -122,10 +145,10 @@ describe Paasal::API::V1::Providers do
       before { post "/vendors/#{vendor.id}/providers", name: updated_name }
       include_examples 'a bad request'
 
-      context 'causes the provider list' do
-        before { get '/providers' }
+      context 'proves that the provider list' do
+        before { get "/vendors/#{vendor.id}/providers" }
         include_examples 'a valid GET request'
-        it 'to not contain more providers than before' do
+        it 'does not contain more providers than before' do
           expect(json_body[:size]).to eq 2
           expect_json_sizes(providers: 2)
         end
