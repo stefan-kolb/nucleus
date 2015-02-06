6767fb17535cdb2cd7fa4a290ea4a2c01ce1f2b9
Cedric Röck <git@roecky.net>
2015-02-06 10:53:34 +0100

    BaseAdapter update (with docu)

--- app/adapters/base_adapter.rb
+++ app/adapters/base_adapter.rb
@@ -1,12 +1,8 @@
-require 'digest'
-
 module Paasal
   module Adapters
     class BaseAdapter
+      include Paasal::Adapters::HttpClient
       include Paasal::Logging
-
-      # thread-based cache for the api authorization headers
-      thread_config_accessor :auth_objects_cache, default: {}
 
       def initialize(endpoint_url, check_certificates = true)
         fail ArgumentError, "'endpoint_url' must be a valid URL" unless endpoint_url =~ /\A#{URI::regexp(['https'])}\z/
@@ -14,11 +10,41 @@ module Paasal
         @check_certificates = check_certificates
       end
 
+      # Get a OAuth2 client for this URL.
+      # @return [Paasal::OAuth2Client] OAuth2Client instance to handle the authentication
+      def oauth2(auth_url)
+        Paasal::OAuth2Client.new(auth_url, @check_certificates)
+      end
+
+      # thread-based cache for the api authorization headers
+      thread_config_accessor :auth_objects_cache, default: {}
+
+      # Cache the auth information.
+      # @param [String] username username for cache key
+      # @param [String] password password for cache key
+      # @param [Hash<String,String>, Paasal::OAuth2Client] auth_object auth information to be cached
+      # @return [void]
       def cache(username, password, auth_object)
         key = cache_key(username, password)
         auth_objects_cache[key] = auth_object
       end
 
+      # Evict the cache for this cache key.
+      # @param [String] key cache to evict
+      def uncache(key)
+        auth_objects_cache.delete key
+      end
+
+      # Are there cached information for the current user?
+      # @param [String] username username for cache lookup
+      # @param [String] password password for cache lookup
+      # @return [true, false] true if has cached auth info, else false
+      def cache?(username, password)
+        auth_objects_cache.key? cache_key(username, password)
+      end
+
+      # Get the cached authentication object, being either the OAuth2Client or the Authorization header.
+      # @return [Hash<String,String>, Paasal::OAuth2Client] cached authentication information
       def headers
         auth_object = auth_objects_cache[RequestStore.store[:cache_key]]
         return auth_object unless auth_object.is_a? Paasal::OAuth2Client
@@ -26,16 +52,14 @@ module Paasal
         auth_object.auth_header
       end
 
-      def uncache(key)
-        auth_objects_cache.delete key
-      end
+      private
 
-      def cache?(username, password)
-        auth_objects_cache.key? cache_key(username, password)
-      end
-
-      def oauth2(auth_url)
-        Paasal::OAuth2Client.new(auth_url, @check_certificates)
+      def cache_key(username, password)
+        # calculate the cache only once per request
+        return RequestStore.store[:cache_key] if RequestStore.exist?(:cache_key)
+        key = Digest::SHA256.hexdigest "#{@endpoint_url}#{username}:#{password}"
+        RequestStore.store[:cache_key] = key
+        key
       end
 
       # def self.method_added(name)
@@ -56,16 +80,6 @@ module Paasal
       #   alias_method name, with_wrapper
       #   @__last_methods_added = nil
       # end
-
-      private
-
-      def cache_key(username, password)
-        # calculate the cache only once per request
-        return RequestStore.store[:cache_key] if RequestStore.exist?(:cache_key)
-        key = Digest::SHA256.hexdigest "#{@endpoint_url}#{username}:#{password}"
-        RequestStore.store[:cache_key] = key
-        key
-      end
     end
   end
 end
