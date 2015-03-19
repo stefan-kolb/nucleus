9f7585a8c56ead6ea47e92caa9d6b1a59a3bf761
Cedric Röck <git@roecky.net>
2015-03-19 11:50:07 +0100

    Added Regexp `PERFECT_URL_PATTERN` since the default pattern caused issues :(

---
+++ spec/unit/common/regexp_spec.rb
@@ -0,0 +1,33 @@
+# encoding: utf-8
+require 'spec/unit/unit_spec_helper'
+require 'lib/regexp'
+
+describe 'Regexp::PERFECT_URL_PATTERN' do
+  %w(
+    http://✪df.ws/123 http://userid:password@example.com:8080 http://userid:password@example.com:8080/
+    http://userid@example.com http://userid@example.com/ http://userid@example.com:8080 http://userid@example.com:8080/
+    http://userid:password@example.com http://userid:password@example.com/ http://142.42.1.1/ http://142.42.1.1:8080/
+    http://➡.ws/䨹 http://⌘.ws http://⌘.ws/ http://foo.com/blah_(wikipedia)#cite-1
+    http://foo.com/blah_(wikipedia)_blah#cite-1 http://foo.com/unicode_(✪)_in_parens
+    http://foo.com/(something)?after=parens http://☺.damowmow.com/ http://code.google.com/events/#&product=browser
+    http://j.mp ftp://foo.bar/baz http://foo.bar/?q=Test%20URL-encoded%20stuff http://مثال.إختبار
+    http://例子.测试).each do |valid_url|
+    it "matches #{valid_url}" do
+      expect(Regexp::PERFECT_URL_PATTERN =~ valid_url).to eq 0
+    end
+  end
+
+  [
+    'http://', 'http://.', 'http://..', 'http://../', 'http://?', 'http://??', 'http://??/', 'http://#', 'http://##',
+    'http://##/', 'http://foo.bar?q=Spaces should be encoded', '//', '//a', '///a', '///', 'http:///a', 'foo.com',
+    'rdar://1234', 'h://test', 'http:// shouldfail.com', ':// should fail', 'http://foo.bar/foo(bar)baz quux',
+    'ftps://foo.bar/', 'http://-error-.invalid/', 'http://a.b--c.de/', 'http://-a.b.co', 'http://a.b-.co',
+    'http://0.0.0.0', 'http://10.1.1.0', 'http://10.1.1.255', 'http://224.1.1.1', 'http://1.1.1.1.1',
+    'http://123.123.123', 'http://3628126748', 'http://.www.foo.bar/', 'http://www.foo.bar./', 'http://.www.foo.bar./',
+    'http://10.1.1.1', 'http://10.1.1.254'
+  ].each do |invalid_url|
+    it "does not match #{invalid_url}" do
+      expect(Regexp::PERFECT_URL_PATTERN =~ invalid_url).to be_nil
+    end
+  end
+end
