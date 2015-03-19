91f62dbfd879ccfe9712f2be41e23ba6ec412b4e
Cedric Röck <git@roecky.net>
2015-03-19 12:57:54 +0100

    HttpClient update

--- app/core/api/adapter/http_client.rb
+++ app/core/api/adapter/http_client.rb
@@ -1,18 +1,36 @@
 module Paasal
   module Adapters
     module HttpClient
+      # Executes a HEAD request to the given URL.
+      #
+      # @param [String] path path to add to the endpoint URL
+      # @param [Hash] params options to call the post request with
+      # @option params [Array<int>] :expects ([200]) http status code that is expected
+      # @option params [Hash] :headers request headers to use with the request
+      # @option params [Boolean] :native_call if true the request is a native API call and shall return the
+      # unprocessed response
+      # @raise [Paasal::Errors::ApiError] if the call failed and did not return the expected code(s)
+      def head(path, params = {})
+        # idempotent: true, retry_limit: 2
+        params[:expects] = [200] unless params.key? :expects
+        params[:method] = :head
+        execute_request(path, params, params.delete(:native_call) { false })
+      end
+
       # Executes a GET request to the given URL.
       #
       # @param [String] path path to add to the endpoint URL
       # @param [Hash] params options to call the post request with
       # @option params [Array<int>] :expects ([200]) http status code that is expected
       # @option params [Hash] :headers request headers to use with the request
+      # @option params [Boolean] :native_call if true the request is a native API call and shall return the
+      # unprocessed response
       # @raise [Paasal::Errors::ApiError] if the call failed and did not return the expected code(s)
       def get(path, params = {})
         # idempotent: true, retry_limit: 2
         params[:expects] = [200] unless params.key? :expects
         params[:method] = :get
-        execute_request(path, params)
+        execute_request(path, params, params.delete(:native_call) { false })
       end
 
       # Executes a POST request to the given URL.
@@ -22,11 +40,13 @@ module Paasal
       # @option params [Array<int>] :expects ([200,201]) http status code that is expected
       # @option params [Hash] :body request body, will be converted to json format
       # @option params [Hash] :headers request headers to use with the request
+      # @option params [Boolean] :native_call if true the request is a native API call and shall return the
+      # unprocessed response
       # @raise [Paasal::Errors::ApiError] if the call failed and did not return the expected code(s)
       def post(path, params = {})
         params[:expects] = [200, 201] unless params.key? :expects
         params[:method] = :post
-        execute_request(path, params)
+        execute_request(path, params, params.delete(:native_call) { false })
       end
 
       # Executes a PATCH request to the given URL.
@@ -36,11 +56,13 @@ module Paasal
       # @option params [Array<int>] :expects ([200,201]) http status code that is expected
       # @option params [Hash] :body request body, will be converted to json format
       # @option params [Hash] :headers request headers to use with the request
+      # @option params [Boolean] :native_call if true the request is a native API call and shall return the
+      # unprocessed response
       # @raise [Paasal::Errors::ApiError] if the call failed and did not return the expected code(s)
       def patch(path, params = {})
         params[:expects] = [200, 201] unless params.key? :expects
         params[:method] = :patch
-        execute_request(path, params)
+        execute_request(path, params, params.delete(:native_call) { false })
       end
 
       # Executes a PUT request to the given URL.
@@ -50,11 +72,13 @@ module Paasal
       # @option params [Array<int>] :expects ([200,201]) http status code that is expected
       # @option params [Hash] :body request body, will be converted to json format
       # @option params [Hash] :headers request headers to use with the request
+      # @option params [Boolean] :native_call if true the request is a native API call and shall return the
+      # unprocessed response
       # @raise [Paasal::Errors::ApiError] if the call failed and did not return the expected code(s)
       def put(path, params = {})
         params[:expects] = [200, 201] unless params.key? :expects
         params[:method] = :put
-        execute_request(path, params)
+        execute_request(path, params, params.delete(:native_call) { false })
       end
 
       # Executes a DELETE request to the given URL.
@@ -63,31 +87,35 @@ module Paasal
       # @param [Hash] params options to call the post request with
       # @option params [Array<int>] :expects ([200,204]) http status code that is expected
       # @option params [Hash] :headers request headers to use with the request
+      # @option params [Boolean] :native_call if true the request is a native API call and shall return the
+      # unprocessed response
       # @raise [Paasal::Errors::ApiError] if the call failed and did not return the expected code(s)
       def delete(path, params = {})
         params[:expects] = [200, 204] unless params.key? :expects
         params[:method] = :delete
-        execute_request(path, params)
+        execute_request(path, params, params.delete(:native_call) { false })
       end
 
       private
 
-      def execute_request(path, params)
-        url = to_url path
-        # log.debug("#{params[:method]} --> #{url}")
-        response = Excon.new(url, excon_connection_params).request(add_common_request_params(params))
-        # log.debug("Response received for request to #{url}")
+      def execute_request(path, params, native_call = false)
+        url = Regexp::PERFECT_URL_PATTERN =~ path ? path : to_url(path)
+        response = Excon.new(url, excon_connection_params(params)).request(add_common_request_params(params))
         # we never want the JSON string, but always the hash representation
         response.body = hash_of(response.body)
         response
       rescue Excon::Errors::HTTPStatusError => e
-        handle_execute_request_error(e, url)
+        handle_execute_request_error(e, url, native_call)
       end
 
-      def handle_execute_request_error(e, url)
+      def handle_execute_request_error(e, url, native_call)
         log.debug 'ERROR, Excon could not execute the request.'
         # transform json response to Hash object
         e.response.body = hash_of(e.response.body)
+
+        # if this is a native API call, do not further process the error
+        return e.response if native_call
+
         # fail with adapter specific error handling
         handle_error(e.response) if respond_to?(:handle_error)
         fallback_error_handling(e, url)
@@ -115,10 +143,15 @@ module Paasal
         "#{@endpoint_url}#{path}"
       end
 
-      def excon_connection_params
+      def excon_connection_params(params)
         middleware = Excon.defaults[:middlewares].dup
-        middleware = [Excon::Middleware::ResponseParser, Excon::Middleware::RedirectFollower,
-                      Excon::Middleware::Decompress].push(*middleware).uniq
+
+        if params[:follow_redirects] == false
+          middleware = [Excon::Middleware::ResponseParser, Excon::Middleware::Decompress].push(*middleware).uniq
+        else
+          middleware = [Excon::Middleware::ResponseParser, Excon::Middleware::RedirectFollower,
+                        Excon::Middleware::Decompress].push(*middleware).uniq
+        end
         { middlewares: middleware, ssl_verify_peer: @check_certificates }
       end
 
@@ -135,7 +168,12 @@ module Paasal
       def add_common_request_params(params)
         common_params = { connection_timeout: 600, write_timeout: 300, read_timeout: 90 }
         # allow to follow redirects in the APIs
-        params[:expects] = [301, 302, 303, 307, 308].push(*params[:expects]).uniq
+        allowed_status_codes = params.key?(:expects) ? [*params[:expects]] : []
+        unless params[:follow_redirects] == false
+          allowed_status_codes.push(*[301, 302, 303, 307, 308])
+        end
+
+        params[:expects] = allowed_status_codes.uniq
         # use default or customized headers
         params[:headers] = headers unless params[:headers]
         # specify encoding if not done yet: use only gzip since deflate does cause issues with VCR cassettes in tests
