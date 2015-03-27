d30229af67ac5af28d6a3d48ca7b944e7049af85
Cedric Röck <git@roecky.net>
2015-03-27 14:56:30 +0100

    log test definitions PT1

---
+++ spec/adapter/support/features/shared_example_adapter_application_logs.rb
@@ -0,0 +1,101 @@
+# NOTE: use only 'paasal-test-app-all-updated' as valid app throughout all tests
+
+shared_examples 'log list schema' do
+  it 'complies with the log list schema' do
+    expect_json_keys(Paasal::API::Models::Logs.documentation.keys)
+  end
+end
+
+shared_examples 'valid:logs:list' do
+  describe 'list logs', :as_cassette do
+    before { get "/endpoints/#{@endpoint}/applications/paasal-test-app-all-updated/logs", request_headers }
+    include_examples 'a valid GET request'
+    include_examples 'log list schema'
+    it 'does contain at least one logfile' do
+      expect(json_body[:logs]).not_to eql([])
+    end
+    it 'contained logfile complies with log entity schema' do
+      expect(json_body[:logs][0].keys).to eql(Paasal::API::Models::Log.documentation.keys)
+    end
+  end
+
+  describe 'list logs fails for non-existing application', :as_cassette do
+    before { get "/endpoints/#{@endpoint}/applications/app_never_exists_0123456789/logs", request_headers }
+    include_examples 'an unknown requested resource'
+  end
+end
+
+# TODO: download build log, content should equal show build log --> test all 3 download formats
+# TODO: download all log files, non-empty archive after deployment (!) --> test both download formats
+# TODO: tail request log (if available), then fire request, then expect new messages, then kill request.
+# Execute within EM.run block? Then use EM.stop
+
+# TODO: shall be retrieved before application deployment
+shared_examples 'valid:logs:get:empty' do
+  describe 'get empty build log', :as_cassette do
+    before do
+      # TODO: at the time of writing this test, we assume that each platform provides a build log.
+      # If this should not be the case, replace the build log with the first element in a queried log list
+      get "/endpoints/#{@endpoint}/applications/paasal-test-app-all-updated/logs/build", request_headers
+    end
+    include_examples 'a valid GET request'
+
+    # TODO: response format text/plain or text/html ?!
+    # TODO: empty response body
+  end
+end
+
+shared_examples 'valid:logs:get' do
+  describe 'get build log', :as_cassette do
+    before do
+      # TODO: at the time of writing this test, we assume that each platform provides a build log.
+      # If this should not be the case, replace the build log with the first element in a queried log list
+      get "/endpoints/#{@endpoint}/applications/paasal-test-app-all-updated/logs/build", request_headers
+    end
+    include_examples 'a valid GET request'
+
+    # TODO: response format text/plain or text/html ?!
+    # TODO: has entries
+  end
+
+  describe 'get log fails for non-existent log_id', :as_cassette do
+    before { get "/endpoints/#{@endpoint}/applications/app_never_exists_0123456789/logs/unknown_id", request_headers }
+    include_examples 'an unknown requested resource'
+  end
+end
+
+
+shared_examples 'valid:applications:logs:tail' do
+  describe 'tail request log', :as_cassette do
+    before do
+      @app = get("/endpoints/#{@endpoint}/applications/paasal-test-app-all-updated", request_headers)
+      @recent = get("/endpoints/#{@endpoint}/applications/paasal-test-app-all-updated/logs/request", request_headers)
+    end
+    it '' do
+      # TODO: at the time of writing this test, we assume that each platform provides a request log.
+      # If this should not be the case, replace the build log with the first element in a queried log list
+
+
+      # EM.run do
+        # TODO: use streaming capable client
+        tail = get "/endpoints/#{@endpoint}/applications/paasal-test-app-all-updated/logs/request/tail", request_headers
+        p tail
+        p body
+        p headers
+
+        # use excon so that the external request is recorded
+        @live_app = Excon.get(@app[:web_url])
+
+        # EM.stop
+      # end
+
+      # TODO: assertions
+
+    end
+
+    # include_examples 'a valid GET request'
+
+    # TODO: response format text/plain or text/html ?!
+    # TODO: new request entries appear
+  end
+end
--- spec/adapter/support/shared_example_adapters_valid_auth.rb
+++ spec/adapter/support/shared_example_adapters_valid_auth.rb
@@ -5,80 +5,84 @@ require 'spec/adapter/support/features/shared_example_adapter_application_scalin
 require 'spec/adapter/support/features/shared_example_adapter_application_states'
 require 'spec/adapter/support/features/shared_example_adapter_application_domains'
 require 'spec/adapter/support/features/shared_example_adapter_application_vars'
+require 'spec/adapter/support/features/shared_example_adapter_application_logs'
 require 'spec/adapter/support/features/shared_example_adapter_regions'
 
 shared_examples 'compliant adapter with valid credentials' do
   describe 'is compliant and' do
     # TODO: shall we prepare the test and delete all applications?
 
-    # region list and get
-    include_examples 'valid:regions:list'
-    include_examples 'valid:regions:get'
+    # # region list and get
+    # include_examples 'valid:regions:list'
+    # include_examples 'valid:regions:get'
 
-    # non-existent application
-    include_examples 'valid:applications:get:404'
-    # create our test application
+    # # non-existent application
+    # include_examples 'valid:applications:get:404'
+    # # create our test application
     include_examples 'valid:applications:create'
-    # create and fail with duplicate name
-    include_examples 'valid:applications:create:422'
-    # get created application
-    include_examples 'valid:applications:get'
-    # application list with entity
-    include_examples 'valid:applications:list'
-    # Update application properties
+    # # create and fail with duplicate name
+    # include_examples 'valid:applications:create:422'
+    # # get created application
+    # include_examples 'valid:applications:get'
+    # # application list with entity
+    # include_examples 'valid:applications:list'
+    # # Update application properties
     include_examples 'valid:applications:update'
-    # failed lifecycle operations when there is no deployment
-    include_examples 'valid:applications:lifecycle:422'
-    # can't download bits when no deployment was done before
-    include_examples 'valid:applications:data:download:422'
-    # can't rebuild when no deployment was done before
-    include_examples 'valid:applications:data:rebuild:422'
-
-    # empty vars list
-    include_examples 'valid:vars:list:empty'
-    include_examples 'valid:vars:get:404'
-    # create fails with bad arguments
-    include_examples 'valid:vars:create:400'
-    include_examples 'valid:vars:create'
-    # create fails with duplicate arguments
-    include_examples 'valid:vars:create:422'
-    include_examples 'valid:vars:update:400'
-    include_examples 'valid:vars:update'
-    include_examples 'valid:vars:update:404'
-    include_examples 'valid:vars:get'
-    # vars list with entity
-    include_examples 'valid:vars:list'
-
-    # empty domain list
-    include_examples 'valid:domains:list:empty'
-    include_examples 'valid:domains:get:404'
-    # create fails with bad arguments
-    include_examples 'valid:domains:create'
-    include_examples 'valid:domains:create:422'
-    include_examples 'valid:domains:get'
-    # domain list with entity
-    include_examples 'valid:domains:list'
-
-    # deploy the application data
+    # # failed lifecycle operations when there is no deployment
+    # include_examples 'valid:applications:lifecycle:422'
+    # # can't download bits when no deployment was done before
+    # include_examples 'valid:applications:data:download:422'
+    # # can't rebuild when no deployment was done before
+    # include_examples 'valid:applications:data:rebuild:422'
+    #
+    # # empty vars list
+    # include_examples 'valid:vars:list:empty'
+    # include_examples 'valid:vars:get:404'
+    # # create fails with bad arguments
+    # include_examples 'valid:vars:create:400'
+    # include_examples 'valid:vars:create'
+    # # create fails with duplicate arguments
+    # include_examples 'valid:vars:create:422'
+    # include_examples 'valid:vars:update:400'
+    # include_examples 'valid:vars:update'
+    # include_examples 'valid:vars:update:404'
+    # include_examples 'valid:vars:get'
+    # # vars list with entity
+    # include_examples 'valid:vars:list'
+    #
+    # # empty domain list
+    # include_examples 'valid:domains:list:empty'
+    # include_examples 'valid:domains:get:404'
+    # # create fails with bad arguments
+    # include_examples 'valid:domains:create'
+    # include_examples 'valid:domains:create:422'
+    # include_examples 'valid:domains:get'
+    # # domain list with entity
+    # include_examples 'valid:domains:list'
+    #
+    # # deploy the application data
     include_examples 'valid:applications:data:deploy'
-
-    # lifecycle operations
+    #
+    # # lifecycle operations
     include_examples 'valid:applications:lifecycle'
+    #
+    # # download the deployed container bits
+    # include_examples 'valid:applications:data:download'
+    # include_examples 'valid:applications:data:rebuild'
+    #
 
-    # download the deployed container bits
-    include_examples 'valid:applications:data:download'
-    include_examples 'valid:applications:data:rebuild'
+    include_examples 'valid:applications:logs:tail'
 
-    # access the application at its URL
-    include_examples 'valid:applications:web'
-
-    # scaling operations
-    include_examples 'valid:applications:scale:400'
-    include_examples 'valid:applications:scale'
-
-    # delete operations
-    include_examples 'valid:domains:delete'
-    include_examples 'valid:vars:delete'
+    # # access the application at its URL
+    # include_examples 'valid:applications:web'
+    #
+    # # scaling operations
+    # include_examples 'valid:applications:scale:400'
+    # include_examples 'valid:applications:scale'
+    #
+    # # delete operations
+    # include_examples 'valid:domains:delete'
+    # include_examples 'valid:vars:delete'
     include_examples 'valid:applications:delete'
 
     # TODO: add log test cases
