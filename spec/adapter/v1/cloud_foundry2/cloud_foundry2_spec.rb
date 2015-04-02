8d697ab59f160bff18877ebc019551e3a6190bb6
Cedric Röck <git@roecky.net>
2015-04-02 11:16:11 +0200

    re-enabled all CF tests

--- spec/adapter/v1/cloud_foundry2/cloud_foundry2_spec.rb
+++ spec/adapter/v1/cloud_foundry2/cloud_foundry2_spec.rb
@@ -9,34 +9,34 @@ describe Paasal::Adapters::V1::CloudFoundry2 do
     @application_params = { memory: 256.to_i }
   end
 
-  # context 'with invalid credentials' do
-  #   let!(:request_headers) { credentials(@endpoint, false) }
-  #   include_examples 'invalid:#authenticate'
-  #   include_examples 'compliant adapter with invalid credentials'
-  # end
-  #
-  # describe 'with missing credentials' do
-  #   let!(:request_headers) { {} }
-  #   include_examples 'compliant adapter with invalid credentials'
-  # end
+  context 'with invalid credentials' do
+    let!(:request_headers) { credentials(@endpoint, false) }
+    include_examples 'invalid:#authenticate'
+    include_examples 'compliant adapter with invalid credentials'
+  end
+
+  describe 'with missing credentials' do
+    let!(:request_headers) { {} }
+    include_examples 'compliant adapter with invalid credentials'
+  end
 
   context 'with valid credentials' do
     let!(:request_headers) { credentials(@endpoint) }
     # include_examples 'valid: OAuth2 #authenticate'
     include_examples 'compliant adapter with valid credentials'
 
-    # describe 'native adapter call' do
-    #   describe 'against endpoint' do
-    #     describe 'does fetch all buildpacks', :as_cassette do
-    #       before do
-    #         get "/endpoints/#{@endpoint}/call/v2/buildpacks", request_headers
-    #       end
-    #       include_examples 'a valid GET request'
-    #       it 'with the specified structure' do
-    #         expect_json_keys(:total_results, :total_pages, :resources)
-    #       end
-    #     end
-    #   end
-    # end
+    describe 'native adapter call' do
+      describe 'against endpoint' do
+        describe 'does fetch all buildpacks', :as_cassette do
+          before do
+            get "/endpoints/#{@endpoint}/call/v2/buildpacks", request_headers
+          end
+          include_examples 'a valid GET request'
+          it 'with the specified structure' do
+            expect_json_keys(:total_results, :total_pages, :resources)
+          end
+        end
+      end
+    end
   end
 end
