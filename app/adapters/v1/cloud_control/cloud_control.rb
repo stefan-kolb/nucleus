31a4fd5f305fd03868f2b1dca45c8e9084a26925
Cedric Röck <git@roecky.net>
2015-04-07 10:00:27 +0200

    cloud control: data operations implemented

--- app/adapters/v1/cloud_control/cloud_control.rb
+++ app/adapters/v1/cloud_control/cloud_control.rb
@@ -2,12 +2,13 @@ module Paasal
   module Adapters
     module V1
       class CloudControl < Stub
+        PAASAL_DEPLOYMENT ||= 'paasal'
+
         include Paasal::Logging
         include Paasal::Adapters::V1::CloudControl::Application
         include Paasal::Adapters::V1::CloudControl::Buildpacks
         include Paasal::Adapters::V1::CloudControl::Domains
         include Paasal::Adapters::V1::CloudControl::Data
-        include Paasal::Adapters::V1::CloudControl::Lifecycle
         include Paasal::Adapters::V1::CloudControl::Logs
         include Paasal::Adapters::V1::CloudControl::Vars
         # all cloud foundry specific semantic errors shall have an error code of 422_6XXX
@@ -67,14 +68,14 @@ module Paasal
         # @see Stub#scale
         def scale(application_id, instances)
           # update the number of instances on the application's deployment
-          scale_response = put("/app/#{application_id}/deployment/paasal", body: { min_boxes: instances }).body
+          scale_response = put("/app/#{application_id}/deployment/#{PAASAL_DEPLOYMENT}",
+                               body: { min_boxes: instances }).body
           to_paasal_app(get("/app/#{application_id}").body, scale_response)
         end
 
         private
 
-        def application_state(application, deployment)
-          # TODO: implement me
+        def application_state(deployment)
           # CloudControl does not create a deployment by default.
           # It also does not support start and stop operations.
           # One workaround is to create (start) and delete (stop) all deployments.
@@ -82,28 +83,22 @@ module Paasal
           # Then its state shall be not deployed, which could be interpreted as equivalent to stopped,
           #  since the deployment code is still in the git repository.
           #
-          # State	Object	Native state	Group	Detection order
-          # created	git	repository is empty	A	1
-          # deployed	git	repository is not empty	A	2
-          # deployed	deployments	array length == 0	A	2
-          # running	deployments	array length > 0 AND at least one: state == deployed	A	4
-          # stopped	deployments	array length > 0 AND for each: state == not deplyoed	A	3
-          # suspended	deployments	for each: state == idle	A	4
-          #
-          # return API::Models::Application::States::CREATED if deployment[:version] == -1
+          # With cloud control not supporting the PaaSal application lifecycle, only 2 actual states remain:<br>
+          # * created, when no data deployment (not to confuse with cloud control deployment object) has been made yet
+          # * running, if a data deployment was pushed
+          return API::Models::Application::States::CREATED if deployment[:version] == '-1'
           # return API::Models::Application::States::DEPLOYED
-          # return API::Models::Application::States::RUNNING
+          return API::Models::Application::States::RUNNING
           # return API::Models::Application::States::STOPPED
           # return API::Models::Application::States::IDLE
 
-          'TO BE DETERMINED'
           # arriving here the above states do not catch all states of the cloudControl app, which should not happen ;-)
           # fail Errors::UnknownAdapterCallError, 'Could not determine application state. '\
           #   'Please verify the cloudControl adapter'
         end
 
         def default_deployment(application_id)
-          get("/app/#{application_id}/deployment/paasal").body
+          get("/app/#{application_id}/deployment/#{PAASAL_DEPLOYMENT}").body
         end
 
         def headers
@@ -123,15 +118,14 @@ module Paasal
           app[:id] = app[:name]
           app[:created_at] = app.delete :date_created
           app[:updated_at] = app.delete :date_modified
-          app[:state] = application_state(app, deployment)
+          app[:state] = application_state(deployment)
           app[:web_url] = deployment[:default_subdomain]
           app[:autoscaled] = false
           app[:region] = 'default'
           app[:instances] = deployment[:min_boxes]
           app[:active_runtime] = app[:type][:name] == 'custom' ? app[:buildpack_url] : app[:type][:name]
           app[:runtimes] = [app[:active_runtime]]
-          # TODO: verify: version == -1 indicates that there was no deployment
-          app[:release_version] = deployment[:version] != -1 ? deployment[:version] : nil
+          app[:release_version] = deployment[:version] != '-1' ? deployment[:version] : nil
           app
         end
       end
--- app/adapters/v1/cloud_control/data.rb
+++ app/adapters/v1/cloud_control/data.rb
@@ -4,47 +4,52 @@ module Paasal
       class CloudControl < Stub
         # cloud control data management operations
         module Data
+
+          # BEWARE:<br>
+          # cloud control violates the PaaSal application lifecycle. After the deployment, the application
+          # is immediately running and can't be stopped!
           # @see Stub#deploy
           def deploy(application_id, file, compression_format)
+            # get deployment, also serves as 404 check for application
             deployment = default_deployment(application_id)
-            account = get('/user').body[0]
-            repo_name = "paasal.app.repo.cloudControl.deploy.#{application_id}.#{SecureRandom.uuid}"
-            # clone, extract, push and finally delete cloned repository (sync)
-
+            user = get('/user').body[0]
+            name = "paasal.app.repo.cloudControl.deploy.#{application_id}.#{SecureRandom.uuid}"
             # push to the deployment branch, here: paasal
-            GitDeployer.new(repo_name, deployment[:branch], account[:email], 'paasal').deploy(file, compression_format)
+            GitDeployer.new(name, deployment[:branch], user[:email], PAASAL_DEPLOYMENT).deploy(file, compression_format)
 
-            # TODO: now deploy via the API
-
-            # TODO: how to stop the application?
+            # now deploy via the API, use version identifier -1 to refer a new build
+            put("app/#{application_id}/deployment/#{PAASAL_DEPLOYMENT}", body: { version: '-1' })
+            # FIXME: application can't be stopped, cloud control does not yet offer lifecycle operations
           end
 
           # @see Stub#download
           def download(application_id, compression_format)
-            # TODO: get deployment state
-            if application_state(app) == API::Models::Application::States::CREATED
+            # get deployment, also serves as 404 check for application
+            deployment = default_deployment(application_id)
+            if application_state(deployment) == API::Models::Application::States::CREATED
               fail Errors::SemanticAdapterRequestError, 'Application must be deployed before data can be downloaded'
             end
 
             # compress files to archive but exclude the .git repo
-            repo_name = "paasal.app.repo.cloudControl.download.#{application_id}.#{SecureRandom.uuid}"
-            GitDeployer.new(repo_name, app[:git_url], nil, 'paasal').download(compression_format, true)
+            name = "paasal.app.repo.cloudControl.download.#{application_id}.#{SecureRandom.uuid}"
+            GitDeployer.new(name, deployment[:branch], nil, PAASAL_DEPLOYMENT).download(compression_format, true)
           end
 
           # @see Stub#rebuild
           def rebuild(application_id)
-            # TODO: get deployment state
-            app = get("/apps/#{application_id}").body
-            if application_state(app) == API::Models::Application::States::CREATED
+            # get deployment, also serves as 404 check for application
+            deployment = default_deployment(application_id)
+            if application_state(deployment) == API::Models::Application::States::CREATED
               fail Errors::SemanticAdapterRequestError, 'Application must be deployed before data can be rebuild'
             end
 
-            account = get('/user').body[0]
-            repo_name = "paasal.app.repo.cloudControl.rebuild.#{application_id}.#{SecureRandom.uuid}"
+            user = get('/user').body[0]
+            name = "paasal.app.repo.cloudControl.rebuild.#{application_id}.#{SecureRandom.uuid}"
 
-            GitDeployer.new(repo_name, app[:git_url], account[:email], 'paasal').trigger_build
+            GitDeployer.new(name, deployment[:branch], user[:email], PAASAL_DEPLOYMENT).trigger_build
 
-            # TODO: now deploy via the API
+            # now deploy via the API, use version identifier -1 to refer a new build
+            put("app/#{application_id}/deployment/#{PAASAL_DEPLOYMENT}", body: { version: '-1' })
 
             # return with updated application
             application(application_id)
