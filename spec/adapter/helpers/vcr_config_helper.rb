6df217e5f61841d685329e6bb7796436909f7ed9
Cedric Röck <git@roecky.net>
2015-04-10 14:51:38 +0200

    Filter data only when VCR is recording, decreases test durations by more than 50%

--- spec/adapter/helpers/vcr_config_helper.rb
+++ spec/adapter/helpers/vcr_config_helper.rb
@@ -65,10 +65,9 @@ VCR.configure do |c|
   def filter_response_body(vcr_config, key)
     vcr_config.filter_sensitive_data("__#{key.underscore.upcase}__") do |i|
       begin
-        response_body = i.response.body.nil? || i.response.body.empty? ? {} : MultiJson.load(i.response.body)
+        response_body = i.response.body.nil? || i.response.body.empty? ? {} : Oj.load(i.response.body)
         if response_body.is_a? Array
-          replacements = response_body.collect { |entry| filter_body(entry, key, 'RESPONSE') }
-          replacements.each_with_index do |replace, index|
+          response_body.collect { |entry| filter_body(entry, key, 'RESPONSE') }.each_with_index do |replace, index|
             # replace sensitive value of the nested element
             vcr_config.filter_sensitive_data("\"__#{key.underscore.upcase}_#{index}__\"") { |_i| replace }
           end
@@ -86,13 +85,15 @@ VCR.configure do |c|
   def filter_request_body(vcr_config, key)
     vcr_config.filter_sensitive_data("__#{key.underscore.upcase}__") do |i|
       # TODO: skipping multipart / ASCII-8BIT requests for now
-      if multipart?(i) && !i.request.body.encoding.to_s == 'ASCII-8BIT'
-        request_body = i.request.body.nil? || i.request.body.empty? ? {} : MultiJson.load(i.request.body)
+      if !multipart?(i) && !i.request.body.encoding.to_s == 'ASCII-8BIT'
+        request_body = i.request.body.nil? || i.request.body.empty? ? {} : Oj.load(i.request.body)
         if request_body.is_a? Array
           request_body.each { |entry| filter_body(entry, key, 'REQUEST') }
         else
           filter_body(request_body, key, 'REQUEST')
         end
+      else
+        "DO_NOT_REPLACE_MULTIPART_AND_BINARY_DATA_AS_OF_#{DateTime.now}"
       end
     end
   end
@@ -101,11 +102,34 @@ VCR.configure do |c|
     i.request.headers['Content-Type'] && !i.request.headers['Content-Type'][0].start_with?('multipart/form')
   end
 
-  %w(token).each { |key| filter_query(c, key) }
-  %w(Authorization).each { |key| filter_header(c, key) }
-  %w(api_key token refresh_token access_token).each do |key|
-    filter_request_body(c, key)
-    filter_response_body(c, key)
+  # only filter these fields when we are recording, otherwise the tests take more than 2x as long
+  unless vcr_record_mode == :none
+    %w(token).each { |key| filter_query(c, key) }
+    %w(Authorization).each { |key| filter_header(c, key) }
+    %w(api_key token refresh_token access_token).each do |key|
+      filter_request_body(c, key)
+      filter_response_body(c, key)
+    end
+
+    # filter heroku user identification
+    c.filter_sensitive_data('__USER_ID__@users.heroku.com') do |i|
+      begin
+        if i.response.body.nil? || i.response.body.empty?
+          "NO_HEROKU_USER_ID_IN_RESPONSE_BODY_TO_REPLACE_AS_OF_#{DateTime.now}"
+        else
+          response_body = Oj.load(i.response.body)
+          response_body['id'] if response_body.is_a?(Hash) && /^\S+@users.heroku.com$/ =~ response_body['id']
+        end
+      rescue
+        "INVALID_JSON_BODY_AS_OF_#{DateTime.now}"
+      end
+    end
+
+    # filter all sensitive data stored in the credentials config
+    Paasal::Spec::Config.credentials.sensitive_data.each do |replacement, replace|
+      # enforce ASCII encoding to prevent VCR from crashing when credentials include umlauts or other characters
+      c.filter_sensitive_data("__#{replacement}__") { |_i| replace.unpack('U*').map(&:chr).join }
+    end
   end
 
   c.register_request_matcher :headers_no_auth do |request_1, request_2|
@@ -160,25 +184,5 @@ VCR.configure do |c|
     valid &&= uri_1.port == uri_2.port
     valid &&= uri_1.fragment == uri_2.fragment
     valid
-  end
-
-  # filter heroku user identification
-  c.filter_sensitive_data('__USER_ID__@users.heroku.com') do |i|
-    begin
-      response_body = i.response.body.nil? || i.response.body.empty? ? {} : MultiJson.load(i.response.body)
-      if response_body.is_a?(Hash) && /^\S+@users.heroku.com$/ =~ response_body['id']
-        response_body['id']
-      else
-        "NO_HEROKU_USER_ID_IN_RESPONSE_BODY_TO_REPLACE_AS_OF_#{DateTime.now}"
-      end
-    rescue
-      "INVALID_JSON_BODY_AS_OF_#{DateTime.now}"
-    end
-  end
-
-  # filter all sensitive data stored in the credentials config
-  Paasal::Spec::Config.credentials.sensitive_data.each do |replacement, replace|
-    # enforce ASCII encoding to prevent VCR from crashing when credentials include umlauts or other characters
-    c.filter_sensitive_data("__#{replacement}__") { |_i| replace.unpack('U*').map(&:chr).join }
   end
 end
