27196e5c4d76e7d8505730324a5c0e7cf3ca2a86
Cedric Röck <git@roecky.net>
2015-04-10 17:41:53 +0200

    cloudControl: log tail updated

--- app/adapters/v1/cloud_control/log_poller.rb
+++ app/adapters/v1/cloud_control/log_poller.rb
@@ -24,39 +24,26 @@ module Paasal
               logs_to_poll.each { |log_to_poll| last_log_entry[log_to_poll] = nil }
 
               fetch_action = lambda do
-                fetched_lines = []
                 logs_to_poll.each do |log_to_poll|
                   # check again if we are still supposed to be active
                   break unless @polling_active
                   lines = @adapter.send(:cc_log_entries, application_name, log_to_poll,
                                         last_log_entry[log_to_poll], @headers_to_use)
                   next if lines.empty?
+                  # now sort by time
+                  lines.sort! { |line_1, line_2| line_1[:time].to_f <=> line_2[:time].to_f }
+                  last_log_entry[log_to_poll] = lines.last[:time] if lines
                   lines.each do |line|
                     line[:paasal_origin] = log_to_poll
-                    fetched_lines.push(line)
+                    stream.send_message(@adapter.send(:format_log_entry, line[:paasal_origin], line))
                   end
-                  last_log_entry[log_to_poll] = Time.at(lines.last[:time]).to_i if lines
                 end
-                push(stream, fetched_lines)
                 # start next iteration if we are still supposed to be active
                 EM.add_timer(timeout) { fetch_action.call } if @polling_active
               end
 
               # start the loop to poll the logs
               EM.add_timer(timeout) { fetch_action.call }
-            end
-
-            # Sort the lines and push them formatted to the stream
-            # @param [RackStreamCallback] stream stream callback to push messages
-            # @param [Array<Hash>] lines unformatted log entries to push to the stream
-            # @return [void]
-            def push(stream, lines)
-              # now sort by time
-              lines.sort! { |line_1, line_2| line_1[:time] <=> line_2[:time] }
-              # now push the sorted lines to the stream
-              lines.each do |line|
-                stream.send_message(@adapter.send(:format_log_entry, line[:paasal_origin], line))
-              end
             end
 
             # Stop the polling at the next shot
