ec249a53d17b6d6799eed19f8574dfd30df5409f
Cedric Röck <git@roecky.net>
2015-04-12 20:02:03 +0200

    Sorted require statements

--- scripts/load_app.rb
+++ scripts/load_app.rb
@@ -1,27 +1,39 @@
 $LOAD_PATH.unshift('lib')
 
+require 'require_all'
+
+# Grape frameworks
 require 'grape'
 require 'grape-entity'
 require 'grape-swagger'
-require 'require_all'
-require 'logger'
-require 'kwalify'
-require 'daybreak'
-require 'lmdb'
-require 'moneta'
+
+# actually more native
 require 'tmpdir'
 require 'find'
 require 'filesize'
 require 'time'
 require 'securerandom'
+require 'logger'
+
+# database
+require 'moneta'
+require 'daybreak'
+require 'lmdb'
+
+# schema
+require 'kwalify'
+
+# serialization
 require 'oj'
 
+# Rack stuff
 require 'rack/body_proxy'
 require 'rack/utils'
 require 'rack/response'
 require 'rack/auth/basic'
 require 'rack/ssl-enforcer'
 
+# Http clients, ...
 # Currently we need excon AND rest_client, due to excon not supporting multipart requests. See also:
 # https://github.com/excon/excon/issues/353
 require 'excon'
@@ -33,6 +45,7 @@ require 'protobuf'
 require 'eventmachine'
 require 'em-http'
 
+# others
 require 'request_store'
 require 'git'
 require 'mime-types'
