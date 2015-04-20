ea5b8cc97770160793e874cb044b92bd22ac2e6a
Cedric Röck <git@roecky.net>
2015-04-20 20:36:13 +0200

    cloudControl: fixed deployment with SSH public keys

--- app/adapters/v1/cloud_control/data.rb
+++ app/adapters/v1/cloud_control/data.rb
@@ -14,7 +14,10 @@ module Paasal
             user = get('/user').body[0]
             name = "paasal.app.repo.cloudControl.deploy.#{application_id}.#{SecureRandom.uuid}"
             # push to the deployment branch, here: paasal
-            GitDeployer.new(name, deployment[:branch], user[:email], PAASAL_DEPLOYMENT).deploy(file, compression_format)
+            with_ssh_key do
+              deployer = GitDeployer.new(name, deployment[:branch], user[:email], PAASAL_DEPLOYMENT)
+              deployer.deploy(file, compression_format)
+            end
 
             # now deploy via the API, use version identifier -1 to refer a new build
             put("app/#{application_id}/deployment/#{PAASAL_DEPLOYMENT}", body: { version: '-1' })
@@ -31,7 +34,9 @@ module Paasal
 
             # compress files to archive but exclude the .git repo
             name = "paasal.app.repo.cloudControl.download.#{application_id}.#{SecureRandom.uuid}"
-            GitDeployer.new(name, deployment[:branch], nil, PAASAL_DEPLOYMENT).download(compression_format, true)
+            with_ssh_key do
+              GitDeployer.new(name, deployment[:branch], nil, PAASAL_DEPLOYMENT).download(compression_format, true)
+            end
           end
 
           # @see Stub#rebuild
@@ -45,13 +50,33 @@ module Paasal
             user = get('/user').body[0]
             name = "paasal.app.repo.cloudControl.rebuild.#{application_id}.#{SecureRandom.uuid}"
 
-            GitDeployer.new(name, deployment[:branch], user[:email], PAASAL_DEPLOYMENT).trigger_build
+            with_ssh_key do
+              GitDeployer.new(name, deployment[:branch], user[:email], PAASAL_DEPLOYMENT).trigger_build
+            end
 
             # now deploy via the API, use version identifier -1 to refer a new build
             put("app/#{application_id}/deployment/#{PAASAL_DEPLOYMENT}", body: { version: '-1' })
 
             # return with updated application
             application(application_id)
+          end
+
+          private
+
+          def with_ssh_key
+            user = username
+            # load ssh key into cloud control
+            matches = configatron.public_key.match(/(.*)\s{1}(.*)\s{1}(.*)/)
+            key_id = register_key(user, matches[1], matches[2])
+            return yield
+          ensure
+            # unload ssh key, allow 404 if the key couldn't be registered at first
+            delete("/user/#{user}/key/#{key_id}") if key_id
+          end
+
+          def register_key(user, type, key)
+            key_name = "paasal-#{SecureRandom.uuid}"
+            post("/user/#{user}/key", body: { key: [type, key, key_name].join(' ') }).body[:key_id]
           end
         end
       end
