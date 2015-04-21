7ad9b3bd807e54ee13d014e4018bcabb92131b1f
Cedric Röck <git@roecky.net>
2015-04-21 10:20:15 +0200

    Recordings update for all adapters

--- spec/adapter/v1/cloud_control/cloud_control_spec.rb
+++ spec/adapter/v1/cloud_control/cloud_control_spec.rb
@@ -21,11 +21,11 @@ describe Paasal::Adapters::V1::CloudControl do
     @api_version = 'v1'
     # we must use these stupid names given that cloud control prohibits special characters and (!)
     # deleted application names are locked for about 48 hours :(
-    @app_min = { original_name: 'paasaltestappminproperties21',
-                         updated_name: 'paasaltestappminproperties21',
+    @app_min = { original_name: 'paasaltestappminproperties29',
+                         updated_name: 'paasaltestappminproperties29',
                          region: 'default' }
-    @app_all = { original_name: 'paasaltestappallproperties21',
-                         updated_name: 'paasaltestappallproperties21',
+    @app_all = { original_name: 'paasaltestappallproperties29',
+                         updated_name: 'paasaltestappallproperties29',
                          region: 'default' }
     # add mysqls with the free plan, does not requier billing account whereas other free plans do !?
     @service = { id: 'mysqls', plan_id: 'free' }
