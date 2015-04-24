af09d1a96d117ac56c8d0c6ba0f0cfbd5234b9e5
Cedric Röck <git@roecky.net>
2015-04-24 11:17:35 +0200

    Updated all recordings with updated versions that now use Oj for serialization and are compatible with Unix and Windows development systems

--- spec/adapter/v1/cloud_control/cloud_control_spec.rb
+++ spec/adapter/v1/cloud_control/cloud_control_spec.rb
@@ -21,11 +21,11 @@ describe Paasal::Adapters::V1::CloudControl do
     @api_version = 'v1'
     # we must use these stupid names given that cloud control prohibits special characters and (!)
     # deleted application names are locked for about 48 hours :(
-    @app_min = { original_name: 'paasaltestappminproperties29',
-                         updated_name: 'paasaltestappminproperties29',
+    @app_min = { original_name: 'paasaltestappminproperties35',
+                         updated_name: 'paasaltestappminproperties35',
                          region: 'default' }
-    @app_all = { original_name: 'paasaltestappallproperties29',
-                         updated_name: 'paasaltestappallproperties29',
+    @app_all = { original_name: 'paasaltestappallproperties35',
+                         updated_name: 'paasaltestappallproperties35',
                          region: 'default' }
     # add mysqls with the free plan, does not requier billing account whereas other free plans do !?
     @service = { id: 'mysqls', plan_id: 'free' }
