36802b8b23a264e0eed219454baf23583f9f6f6a
Cedric Röck <git@roecky.net>
2015-05-18 12:52:30 +0200

    spec: updated cloudControl recordings after SSH handling change

--- spec/adapter/v1/cloud_control/cloud_control_spec.rb
+++ spec/adapter/v1/cloud_control/cloud_control_spec.rb
@@ -25,11 +25,11 @@ describe Paasal::Adapters::V1::CloudControl do
     @api_version = 'v1'
     # we must use these stupid names given that cloud control prohibits special characters and (!)
     # deleted application names are locked for about 48 hours :(
-    @app_min = { original_name: 'paasaltestappminproperties62',
-                         updated_name: 'paasaltestappminproperties62',
+    @app_min = { original_name: 'paasaltestappminproperties66',
+                         updated_name: 'paasaltestappminproperties66',
                          region: 'default' }
-    @app_all = { original_name: 'paasaltestappallproperties62',
-                         updated_name: 'paasaltestappallproperties62',
+    @app_all = { original_name: 'paasaltestappallproperties66',
+                         updated_name: 'paasaltestappallproperties66',
                          region: 'default' }
     # add mysqls with the free plan, does not requier billing account whereas other free plans do !?
     @service = { id: 'mysqls', plan_id: 'free' }
