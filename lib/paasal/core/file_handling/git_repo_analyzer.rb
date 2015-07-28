b1930e5d5d19d026a1cfba3501ee51154f92b01b
Cedric Röck <git@roecky.net>
2015-07-28 13:25:52 +0200

    Bugfix that caused deployed Git files not to be detected, fixes #38

--- lib/paasal/core/file_handling/git_repo_analyzer.rb
+++ lib/paasal/core/file_handling/git_repo_analyzer.rb
@@ -7,16 +7,16 @@ module Paasal
       # @param [String] username user to authenticate with
       # @return [TrueClass, FalseClass] true if the repository has any non-empty branch, e.g. 'master'
       def self.any_branch?(repo_host, repo_name, username)
-        repository_branches = nil
+        detected_branch = false
         options = { forward_agent: true, auth_methods: ['publickey'],
                     keys: [paasal_config.ssh.handler.key_file], keys_only: true }
         Net::SSH.start(repo_host, username, options) do |ssh|
           ssh.exec! "git-upload-pack '/#{repo_name}.git'" do |ch, stream, data|
-            repository_branches = data unless stream == :stderr
+            detected_branch = (detected_branch || data != '0000') unless stream == :stderr
             ch.close
           end
         end
-        repository_branches != '0000'
+        detected_branch
       end
     end
   end
